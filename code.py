110f7c68bbb3d82f5d01a65cd50fdd73044b2ed1
Laura Miller <45724537+laurammiller@users.noreply.github.com>
2024-12-24 14:26:23 -0500

    8 xmas clips

--- code.py
+++ code.py
@@ -56,46 +56,112 @@ mp3file = "EPBlue8.mp3"
 mp3 = open(path + mp3file, "rb")
 decoder = MP3Decoder(mp3)
 
-def play(filename):
+def play(filename, function):
     with open(path + filename, "rb") as mp3:
         decoder.file = mp3
         audio.play(decoder)
-
+        while audio.playing:
+            pass
+            if function == 1:
+                bluechaser()
+            elif function == 2:
+                rainbow(.1)
+            elif function == 3:
+                redgreen()
+            elif function == 4:
+                candycane()
+            elif function == 5:
+                insane()
+            elif function == 6:
+                grinch()
+            elif function == 7:
+                snow()
 def color_chase(color, wait):
     for i in range(num_pixels):
         pixels[i] = color
         time.sleep(wait)
         pixels.show()
-    time.sleep(0.5)
+
 BLUE = (0, 255, 0)
 RED = (0, 0, 255)
 WHITE = (255, 255, 255)
 GREEN = (255, 0, 0)
+PINK = (0, 50, 200)
+
+def rainbow(speed):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels[i] = colorwheel(pixel_index & 255)
+            time.sleep(speed)
+        pixels.show()
+
+def bluechaser():
+    color_chase(WHITE, 0.01)
+    color_chase(BLUE, 0.02)
+
+def redgreen():
+    color_chase(RED, 0.02)
+    color_chase(GREEN, 0.02)
+
+def candycane():
+    color_chase(RED, 0.02)
+    color_chase(WHITE, 0.02)
+    color_chase(PINK, 0.02)
+
+def insane():
+    pixels.fill(RED)
+    time.sleep(0.02)
+    pixels.fill(WHITE)
+    time.sleep(0.02)
+
+def grinch():
+    pixels.fill(GREEN)
+    time.sleep(0.02)
+    pixels.fill(WHITE)
+    time.sleep(0.02)
+    color_chase(GREEN, 0.02)
+    pixels.fill(GREEN)
+    time.sleep(0.04)
+    pixels.fill(WHITE)
+    time.sleep(0.02)
+
+def snow():
+    pixels.fill(WHITE)
+    time.sleep(0.05)
+    pixels.fill(BLUE)
+    time.sleep(0.05)
+Flag = False
 
 while True:
     gesture = apds.gesture()
+    Flag = not Flag
 
     if gesture == 0x01:
         print("up")
-        color_chase(BLUE, 0.01)
-        #play("EPBlue8.mp3")
-        mp3 = open(path + "EPBlue8.mp3", "rb")
-        decoder.file = mp3
-        audio.play(decoder)
-        while audio.playing:
-            pass
-            color_chase(WHITE, 0.1)
-            color_chase(BLUE, 0.1)
+        if Flag:
+            play("EPBlue8.mp3", 1)
+        else:
+            play("Rockin.mp3", 1)
     elif gesture == 0x02:
         print("down")
         pixels.fill((0, 0, 255))
-        play_sound("wand.wav")
+        if Flag:
+            play("wizard.mp3", 5)
+        else:
+            play("grinch.mp3", 6)
     elif gesture == 0x03:
         print("left")
         pixels.fill((0, 255, 0))
-        play_sound("encouragement1.wav")
+        if Flag:
+            play("JingleRock.mp3", 4)
+        else:
+            play("Bing.mp3", 7)
     elif gesture == 0x04:
         print("right")
         pixels.fill((255, 0, 0))
-        play_sound("encouragement2.wav")
+        if Flag:
+            play("feliz.mp3", 3)
+        else:
+            play("maria.mp3", 4)
 # Write your code here :-)
